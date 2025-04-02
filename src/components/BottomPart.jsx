ec7e806ab6f3a2f1a5ef16e9192c76301cc2198f
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2025-04-02 19:38:11 +0100

    finalyse bottom part component

--- src/components/BottomPart.jsx
+++ src/components/BottomPart.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-export default function BottomPart({onClick}) {
+export default function BottomPart({ getRecipe }) {
+  
   return (
       <>
         <section className='buttom-section'>
             <h3> Ready for a recipe ?</h3>
             <div>
               <span> generate a recipe from your list of ingredients</span>
-              <button onClick={onClick}> Get recipe </button>
+              <button onClick={getRecipe}> Get recipe </button>
             </div>
           </section>
       </>
