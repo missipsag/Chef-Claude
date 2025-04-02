44fff208af9d2f3214a15af12e9d840bad74eb3b
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2025-04-02 19:34:25 +0100

    add some components hiearchy

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@
 import './App.css'
 import Header from './components/Header'
 import Form from './components/Form'
-function App() {
-  
+import ToggleSuggestions from './components/ToggleSuggestions'
+import { useState } from 'react'
+import IngredientsPart from './components/IngredientsPart'
+import BottomPart from './components/BottomPart';
+import { getRecipeFromChefClaude } from '../ai';
 
+
+function App() {
+  const [ingredients, setIngredients] = useState(["potatoes", "steak", "salad", "cheez"]);
+  const [recipe, setRecipe] = useState("")
+  async function getRecipe() {
+    const markdownRecipe = await getRecipeFromChefClaude(ingredients);
+    setRecipe(markdownRecipe)    
+  }
+  
   return (
     <>   
       <Header />
-      <Form />
+      <Form ingredients={ingredients} setIngredients={setIngredients} />
+      <IngredientsPart ingredients={ingredients} />
+      {ingredients.length >= 4 && <BottomPart setIsShown={setRecipe} getRecipe={getRecipe}/> }   
+      { recipe.length > 0 && <ToggleSuggestions  recipe ={recipe} />}
     </>
   )
 }
-
 export default App
