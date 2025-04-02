60c2b54e8b613e6a32413e34c1eb78e96c535021
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2025-04-02 15:54:55 +0100

    added env variables config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv"
 
 // https://vite.dev/config/
-export default defineConfig({
+
+export default  defineConfig({
   plugins: [react()],
+  
 })
+
